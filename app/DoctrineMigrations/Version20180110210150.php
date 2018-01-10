1013dea7df2bbee48e541b023cfe026837881ae8
Karim <abdounikarim@gmail.com>
2018-01-10 22:05:09 +0100

    adding genus_note entity and fixtures

---
+++ app/DoctrineMigrations/Version20180110210150.php
@@ -0,0 +1,28 @@
+<?php declare(strict_types = 1);
+
+namespace Application\Migrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+class Version20180110210150 extends AbstractMigration
+{
+    public function up(Schema $schema)
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE genus_note CHANGE user_avater_filename user_avatar_filename VARCHAR(255) NOT NULL');
+    }
+
+    public function down(Schema $schema)
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE genus_note CHANGE user_avatar_filename user_avater_filename VARCHAR(255) NOT NULL COLLATE utf8_unicode_ci');
+    }
+}
