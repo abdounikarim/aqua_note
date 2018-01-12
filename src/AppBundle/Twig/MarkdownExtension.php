f261ca0667c18950a7dc65741b7552cdc646b240
Karim <abdounikarim@gmail.com>
2018-01-12 19:48:38 +0100

    add twig extension

---
+++ src/AppBundle/Twig/MarkdownExtension.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace AppBundle\Twig;
+
+class MarkdownExtension extends \Twig_Extension
+{
+    public function getName()
+    {
+        return 'app_markdown';
+    }
+    public function getFilters()
+    {
+        return new \Twig_SimpleFilter('markdownify', [
+            $this,
+            'parseMarkdown'
+        ]);
+    }
+
+    public function parseMarkdown($str)
+    {
+        return strtoupper($str);
+    }
+
+}
