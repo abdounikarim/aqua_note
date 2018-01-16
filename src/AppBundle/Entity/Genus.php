83c66194967f7d965137cb35f0d0834bb815f95b
Karim <abdounikarim@gmail.com>
2018-01-16 18:29:03 +0100

    adding save and redirect for new genus

--- src/AppBundle/Entity/Genus.php
+++ src/AppBundle/Entity/Genus.php
@@ -63,6 +63,14 @@ class Genus
     /**
      * @return mixed
      */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return mixed
+     */
     public function getName()
     {
         return $this->name;
