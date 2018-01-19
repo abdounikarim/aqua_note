68e9f9035b3e92bc7d000d1f98adedf19c61dcb8
Karim <abdounikarim@gmail.com>
2018-01-19 18:42:07 +0100

    render login form

---
+++ src/AppBundle/Controller/SecurityController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use AppBundle\Form\LoginForm;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class SecurityController extends Controller
+{
+    /**
+     * @Route("/login", name="security_login")
+     */
+    public function loginAction()
+    {
+        $authenticationUtils = $this->get('security.authentication_utils');
+
+        $error = $authenticationUtils->getLastAuthenticationError();
+
+        $lastUsername = $authenticationUtils->getLastUsername();
+
+        $form = $this->createForm(LoginForm::class, [
+            '_username' => $lastUsername
+        ]);
+
+        return $this->render('security/login.html.twig', [
+            'form' => $form->createView(),
+            'error' => $error
+        ]);
+    }
+}
