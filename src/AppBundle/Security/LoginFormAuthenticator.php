980d1dc5f8bedbb2d407d731a45a48804cc0e64a
Karim <abdounikarim@gmail.com>
2018-01-22 11:07:04 +0100

    add path logout and get last username

--- src/AppBundle/Security/LoginFormAuthenticator.php
+++ src/AppBundle/Security/LoginFormAuthenticator.php
@@ -8,6 +8,7 @@ use Symfony\Component\Form\FormFactoryInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\RouterInterface;
 use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use Symfony\Component\Security\Core\Security;
 use Symfony\Component\Security\Core\User\UserInterface;
 use Symfony\Component\Security\Core\User\UserProviderInterface;
 use Symfony\Component\Security\Guard\Authenticator\AbstractFormLoginAuthenticator;
@@ -38,6 +39,12 @@ class LoginFormAuthenticator extends AbstractFormLoginAuthenticator
         $form = $this->formFactory->create(LoginForm::class);
         $form->handleRequest($request);
         $data = $form->getData();
+
+        $request->getSession()->set(
+            Security::LAST_USERNAME,
+            $data['_username']
+        );
+
         return $data;
     }
 
